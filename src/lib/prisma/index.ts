9fe2ff30f7cbc82ab8714c48f6d6ca29a30b44f6
mono0218 <mono@monokamo.com>
2025-01-01 00:23:23 +0900

    WIP: Prisma Accelerate

--- src/lib/prisma/index.ts
+++ src/lib/prisma/index.ts
@@ -1,14 +1,16 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from "@prisma/client/edge"
 import { withAccelerate } from '@prisma/extension-accelerate'
-import {env} from "hono/adapter";
-
 
 export function getPrismaClient(){
-    return new PrismaClient({
-        datasources: {
-            db: {
-                url: "prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiZTZjMjExNmMtNzQxYS00MTIzLWE5NzctNzY4MmZmMjUzYjg3IiwidGVuYW50X2lkIjoiZmJlMGY5MjI0YjQyMzlkNDdkYjc4NmE2NGVjNDY1ZGVlNjEzMmExOGQ2NDY5MmFiNTI0N2NiOGU5MGY2ZDBkNyIsImludGVybmFsX3NlY3JldCI6ImU0OWE1Y2Q1LWM1ZDctNDU2OC1iYzk1LTU3MjMxYmM4MzBhMiJ9.0wJgkwcLb_NZ8Dz8JeY0ssaaTqzNEoU0SIH3xSL2UIM"
+    require('dotenv').config()
+    return new PrismaClient(
+        {
+            datasources: {
+                db: {
+                    url: process.env.DIRECT_DATABASE_URL,
+
+                }
             }
         }
-    }).$extends(withAccelerate())
+    ).$extends(withAccelerate())
 }
